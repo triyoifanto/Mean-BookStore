c3397da037086346041d79778ea4cd2ab21f672a
Amelia Rahman <Amelia_R@mitrais.com>
2016-04-21 17:18:42 +0800

    fixing prod controller

--- modules/products/client/controllers/product.client.controller.js
+++ modules/products/client/controllers/product.client.controller.js
@@ -5,16 +5,6 @@ angular.module('products.admin').controller('ProductController', ['$scope', '$st
     $scope.authentication = Authentication;
     $scope.product = productResolve;
 
-    $scope.find = function() {
-      $scope.articles = Products.query();
-    };
-
-    $scope.findOne = Products() {
-      $scope.article = Articles.get({
-        productId: $routeParams.productId
-      });
-    };
-
     $scope.create = function(isValid){
       if(!isValid){
         $scope.$broadcase('show-errors-check-validity', 'productForm');
@@ -23,22 +13,7 @@ angular.module('products.admin').controller('ProductController', ['$scope', '$st
       }
 
       // TODO: state go to product form
-      var product = new Products({
-        title: this.title,
-        author: this.author,
-        description: this.description,
-        price: this.price,
-        category: this.category,
-        stock: this.stock,
-        image: this.image,
-        status: this.status
-      });
-
-      product.$save(function(response) {
-        $location.path('product/' + response._id);
-      }, function(errorResponse) {
-        $scope.error = errorResponse.data.message;
-      });
+      
     };
 
     $scope.remove = function (product) {
