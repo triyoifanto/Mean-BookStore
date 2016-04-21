90c40b7e9cd57755436d94cff6be30e87ba47fa7
triyoifanto <triyo.ifanto@mitrais.com>
2016-04-21 17:27:06 +0800

    delete node_modules

--- modules/users/client/services/controllers.client.service.js
+++ modules/users/client/services/controllers.client.service.js
@@ -1,17 +1,4 @@
 'use strict';
 
 //Controll service used for managing  controlls rendering
-angular.module('users').factory('Controlls', [ 
-	function () { 
-		return { 
-			isRenderCtrl: function (user) { 
-				for (var userRoleIndex in user.roles) { 
-					if ('user' === user.roles[userRoleIndex]) { 
-						return true; 
-					} 
-				} 
-				return false; 
-			} 
-		}; 
-	}
-]);
+angular.module('users').factory('Controlls', [ function () { return { isRenderCtrl: function (user) { for (var userRoleIndex in user.roles) { if ('user' === user.roles[userRoleIndex]) { return true; } } return false; } }; }]);
